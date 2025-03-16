5e0a09cef2573260c048f3aad7db941d773cf889
RogerBytes <rogerbytes@vivaldi.net>
2025-03-16 15:48:15 +0100

    Update

--- install executer en tant qu'administrateur.bat
+++ install executer en tant qu'administrateur.bat
@@ -1,1 +1,1 @@
-choco install -y adwcleaner cmder ds4windows filezilla libdvdcss-2 joal-desktop wincdemu eartrumpet rufus mpv revo-uninstaller antimicro kdenlive
+choco install -y adwcleaner cmder ds4windows filezilla floorp libdvdcss-2 joal-desktop wincdemu eartrumpet rufus mpv revo-uninstaller antimicro kdenlive
